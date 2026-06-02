43714ffd36d033a99b4afde345dc23a0ac4df164
Aayush895 <aayush895@gmail.com>
2026-06-02 23:07:41 +0530

    Added directory information method

---
+++ functions/get_files_info.py
@@ -0,0 +1,31 @@
+import os
+
+def get_files_info(working_directory: str, directory: str = ".") -> str:
+  try:
+    working_dir_abs = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+    # Will be True or False
+    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+    if(valid_target_dir == False):
+      return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if(os.path.isdir(target_dir) == False):
+      return f'Error: "{directory}" is not a directory'
+
+    fileContentList = [];
+    for fileContent in os.listdir(target_dir):
+      fileContentList.append({
+        "name": fileContent,
+        "size": os.path.getsize(os.path.join(target_dir, fileContent)),
+        "is_dir": os.path.isdir(os.path.join(target_dir, fileContent))
+      })
+
+    fileInfo = ''
+    for content in fileContentList:
+      str = f"- {content["name"]}: file_size={content["size"]} bytes, is_dir={content["is_dir"]}"
+      fileInfo += str
+
+    return fileInfo
+  except RuntimeError:
+    raise RuntimeError('Error: Something went wrong!')
+  
+  
